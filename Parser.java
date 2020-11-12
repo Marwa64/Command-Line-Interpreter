6084f8a0449ff2f566af07d5c553e07d66b68db5
samar <samareeyassin155@gmail.com>
2020-11-12 05:40:40 +0200

    added  cat & "more" & getAbsolutePath functions and updated rm and rmdir

--- Parser.java
+++ Parser.java
@@ -8,7 +8,7 @@ public class Parser {
 		String[] inputArray; 
 		inputArray = input.split(" ");
 		// Checks if the command is valid
-		if (!inputArray[0].equals("cd") && !inputArray[0].equals("ls") && !inputArray[0].equals("cp") && !inputArray[0].equals("mv") && !inputArray[0].equals("rm")  && !inputArray[0].equals("mkdir") && !inputArray[0].equals("rmdir") && !inputArray[0].equals("cat") && !inputArray[0].equals("more") && !inputArray[0].equals("pwd") && !inputArray[0].equals("args") && !inputArray[0].equals("date") && !inputArray[0].equals("help")) {
+		if (!inputArray[0].equals("cd") && !inputArray[0].equals("more") && !inputArray[0].equals("ls") && !inputArray[0].equals("cp") && !inputArray[0].equals("mv") && !inputArray[0].equals("rm")  && !inputArray[0].equals("mkdir") && !inputArray[0].equals("rmdir") && !inputArray[0].equals("cat") && !inputArray[0].equals("more") && !inputArray[0].equals("pwd") && !inputArray[0].equals("args") && !inputArray[0].equals("date") && !inputArray[0].equals("help")) {
 			System.out.println("Command '" + inputArray[0] +"' not found");
 			return false;
 		} else {
@@ -20,7 +20,7 @@ public class Parser {
 				}
 			}
 			// If the command should have at least 1 argument
-			if (inputArray[0].equals("mkdir") || inputArray[0].equals("rmdir") || inputArray[0].equals("rm") || inputArray[0].equals("cat") || inputArray[0].equals("more")) { 
+			if (inputArray[0].equals("mkdir")  || inputArray[0].equals("more") || inputArray[0].equals("rmdir") || inputArray[0].equals("rm") || inputArray[0].equals("cat") || inputArray[0].equals("more")) {
 				if (inputArray.length < 2) {
 					System.out.println("Invalid number of arguments");
 					return false;
--- Terminal.java
+++ Terminal.java
@@ -1,25 +1,15 @@
+import java.io.*;
 import java.util.*;
 import java.util.Scanner;
-import java.io.File;
 
 public class Terminal {
 	static Scanner in = new Scanner(System.in);
 	static Parser parser = new Parser();
 	static String currentDir = "C:\\", defaultDir = "C:\\";
 
-	public static void cp(String sourcePath, String destinationPath) {
 
-	}
 
-	;
-
-	public static void mv(String sourcePath, String destinationPath) {
-
-	}
-
-	;
-
-	public static void rmdir(String inputPath) {
+	public static String getAbsolutePath(String inputPath){
 		String abspath = inputPath;
 		inputPath = inputPath.toLowerCase();
 		String current = currentDir.toLowerCase();
@@ -27,10 +17,65 @@ public class Terminal {
 		if (!(inputPath.contains(current))) {
 			abspath = currentDir + '\\' + inputPath;
 		}
-		File toDelete = new File(abspath);
+		return abspath;
+	}
 
-		recursiveDelete(toDelete);
+	public static void cat(String inputPath) throws FileNotFoundException {
 
+		String absPath = getAbsolutePath(inputPath);
+		File toRead = new File(absPath);
+
+		Scanner scanner = new Scanner(toRead);
+
+		if (toRead.exists()) {
+			while(scanner.hasNextLine())
+			{
+				System.out.println(scanner.nextLine());
+			}
+		}
+		else System.out.println("No such folder or file");
+
+
+	}
+
+	public static void more(String inputPath) throws FileNotFoundException {
+		String absPath = getAbsolutePath(inputPath);
+		File toRead = new File(absPath);
+		Scanner scanner = new Scanner(toRead);
+		int counter =10;
+		if (toRead.exists()) {
+
+			while (scanner.hasNextLine()) {
+				System.out.println(scanner.nextLine());
+				counter--;
+				if (counter == 0) {
+					counter = in.nextInt();
+				}
+			}
+		}
+		else System.out.println("No such folder or file");
+
+
+	}
+
+	public static void cp(String sourcePath, String destinationPath) {
+
+	}
+
+
+
+	public static void mv(String sourcePath, String destinationPath) {
+
+	}
+
+
+
+	public static void rmdir(String inputPath) {
+		String absPath = getAbsolutePath(inputPath);
+		File toDelete = new File(absPath);
+		if(toDelete.exists())
+			recursiveDelete(toDelete);
+		else System.out.println("No such folder or file");
 
 	}
 
@@ -53,30 +98,24 @@ public class Terminal {
 
 
 		public static void rm(String inputPath) {
+		String absPath = getAbsolutePath(inputPath);
 
-			String abspath = inputPath;
-			inputPath = inputPath.toLowerCase();
-			String current = currentDir.toLowerCase();
+		File toDelete= new File(absPath);
 
-			if (!(inputPath.contains(current))) {
-				abspath = currentDir + '\\' + inputPath;
+
+		if (!toDelete.isDirectory() && toDelete.exists()) {
+				if (toDelete.delete()) {
+					System.out.println("Deleted the file: " + toDelete.getName());
+				}
 			}
-		File toDelete= new File(abspath);
-		if(!toDelete.isDirectory() && toDelete.exists()) {
-			if (toDelete.delete()) {
-				System.out.println("Deleted the file: " + toDelete.getName());
-			} else {
-				System.out.println("Failed to delete the file.");
-			}
-		}
-	};
+		else System.out.println("No such file");
+	}
+
 	// Prints the current directory
 	public static void pwd() {
 		System.out.println(currentDir);
-	};
-	public static void cat(String[] paths) {
-		
-	};
+	}
+
 	// Lists all the files/folders in the current or given directory
 	public static void ls(String[] arguments) {
 		File dir;
@@ -88,14 +127,14 @@ public class Terminal {
 		// If there are any errors caught, that means the directory the user entered is incorrect
 		try {
 			File allFiles[] = dir.listFiles();
-	        for (int i = 0; i < allFiles.length; i++) {
+			for (int i = 0; i < allFiles.length; i++) {
 	           System.out.print(allFiles[i].getName() + "  ");
 	        }
 		} catch (Exception e) {
 			System.out.print("Cannot access '"+ arguments[0] + "' : No such file or directory");
 		}
         System.out.println("\n");
-	};
+	}
 	// Changes the current directory
 	public static void cd(String[] arguments) {
 		String userPath, actualPath;
@@ -123,7 +162,7 @@ public class Terminal {
 
 	// Add any other required command in the same structure
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		boolean run = true;
 		String cmd;
 		String [] arg;
@@ -159,7 +198,10 @@ public class Terminal {
 						pwd();
 						break;
 					case "cat":
-						cat(arg);
+						cat(arg[0]);
+						break;
+					case "more":
+						more(arg[0]);
 						break;
 					case "ls":
 						ls(arg);
